793abd19bdb8c0e9cb62d31d9650fb0e7bda74e6
jbgaard <jxras11@hotmail.com>
2026-01-14 15:05:06 +0100

    Updates

---
+++ _app/immutable/chunks/BJkwYtuY.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_3vk7oc?.base??"/missefar-github-pages",a=globalThis.__sveltekit_3vk7oc?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/DT-cyWzU.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1kwyump?.base??"/missefar-github-pages",a=globalThis.__sveltekit_1kwyump?.assets??s??"";export{a,s as b};
