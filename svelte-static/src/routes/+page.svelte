cc95543880460ba3d123921e27e7f8d29b55c31e
jbgaard <jxras11@hotmail.com>
2026-01-14 13:47:48 +0100

    initial commit

--- svelte-static/src/routes/+page.svelte
+++ svelte-static/src/routes/+page.svelte
@@ -1,2 +1,98 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex items-center justify-center">
+	<main class="m-4 max-w-2xl">
+		<h1 class="my-4 text-6xl font-black">EFTERLYST!</h1>
+
+		<img src="/images/DSC01636.jpg" alt="Vores meget elskede kat" class="max-w-lg" />
+
+		<p>Vores søde lille missekat er desværre forsvundet :(</p>
+
+		<ul class="my-2 ml-10 max-w-md list-disc">
+			<li>
+				Han elsker når man kalder ham for <strong><em>missefar</em></strong>, så bliver han meget
+				kælen.
+			</li>
+			<li>Sidst vi tjekkede var en udlært smed d. 16/01/2026</li>
+			<li>Hans kaldenavn i byen er <strong><em>fede flemme fedtbjerg</em></strong></li>
+			<li>
+				Sidst kendte adresse er <a href="https://maps.app.goo.gl/fkFBX9Sx5oKWgzF7A"
+					>Ved Åen 1, 8370 Hadsten</a
+				> eller nærmeste kineser buffet
+			</li>
+			<li>Hans yndlingsdrink er æselspark, børge, fortov eller øl</li>
+			<li>
+				Han er en dårlig taber og sutter kæmpe til billard, meyer, snyd, snyd (med kort), pool, 21,
+				bowling...
+			</li>
+			<li>Drømmerejse er at tage til Thailand med Rolf og hans boytoy</li>
+		</ul>
+
+		<p>
+			Har du set ham? Så kontakt mig hurtigst muligt på <a href="mailto:kontakt@skat.dk"
+				>missefarhartissetpåmig@gmail.com</a
+			>
+		</p>
+
+		<br />
+
+		<h3 class="text-2xl">Galleri:</h3>
+
+		<div class="grid grid-cols-2 gap-4">
+			<video autoplay loop muted class="h-full w-sm object-cover">
+				<source src="/images/dansemusen.mp4" type="video/mp4" />
+				Your browser does not support the video tag.
+			</video>
+			<img
+				src="/images/569945653_877970694820508_2337974435288838911_n.jpg"
+				alt="Vores meget elskede kat"
+				class="h-full w-sm object-cover"
+			/>
+			<img
+				src="/images/592052706_889704966963593_483529784538562711_n.jpg"
+				alt="Vores meget elskede kat"
+				class="h-full w-sm object-cover"
+			/>
+			<img
+				src="/images/611293481_892735893309409_9042629071886184921_n.jpg"
+				alt="Vores meget elskede kat"
+				class="h-full w-sm object-cover"
+			/><img
+				src="/images/611624809_2073225910109181_2452048730345598000_n.jpg"
+				alt="Vores meget elskede kat"
+				class="h-full w-sm object-cover"
+			/><img
+				src="/images/612989588_1381715063652524_3401840366704835540_n.jpg"
+				alt="Vores meget elskede kat"
+				class="h-full w-sm object-cover"
+			/><img
+				src="/images/613049142_1535794591059309_8905850359040993510_n.jpg"
+				alt="Vores meget elskede kat"
+				class="h-full w-sm object-cover"
+			/>
+			<img
+				src="/images/614798545_1448836893624982_1944972222820527643_n.jpg"
+				alt="Vores meget elskede kat"
+				class="h-full w-sm object-cover"
+			/>
+			<img
+				src="/images/Snapchat-469831533.jpg"
+				alt="Vores meget elskede kat"
+				class="h-full w-sm object-cover"
+			/>
+			<img
+				src="/images/DSC01643.jpg"
+				alt="Vores meget elskede kat"
+				class="h-full w-sm object-cover"
+			/>
+			<img
+				src="/images/DSC01644.jpg"
+				alt="Vores meget elskede kat"
+				class="h-full w-sm object-cover"
+			/>
+			<img
+				src="/images/DSC01645.jpg"
+				alt="Vores meget elskede kat"
+				class="h-full w-sm object-cover"
+			/>
+		</div>
+	</main>
+</div>
